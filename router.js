dfe521718bdad628da65f458b42d1a85a156e76a
Jonathan Daly <dalyj138@gmail.com>
2019-05-11 11:06:03 -0400

    Completed GET requests for /api/posts && /api/posts/:id.

---
+++ router.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const DB = require('./data/db.js');
+
+const router = express.Router();
+
+router.get('/', async(req, res) => {
+    
+    try {
+        const posts = await DB.find(req.query);
+        res.status(200).json(posts);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({ error: "The posts information could not be retrieved." });
+    }
+});
+
+router.get('/:id', async(req, res) => {
+
+    try {
+        const post = await DB.findById(req.params.id);
+
+        if(post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." });
+        }
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({ error: "The post information could not be retrieved." });
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
+const Router = require('./router.js');
+
 const server = express();
 
 server.use(express.json());
+server.use('/api/posts', Router);
 
 server.get('/', (req, res) => {
     res.send(`
